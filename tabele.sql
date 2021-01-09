3b2352dbeccb477ea2d97b22ceb0145093c10f12
zuzbrzo <zbrzoska22@gmail.com>
2021-01-09 16:02:59 +0100

    Szkielet bazy z tabelami

---
+++ tabele.sql
@@ -0,0 +1,93 @@
+CREATE TABLE Uczestnicy(
+				Uczestnik_id 		SERIAL,
+				Imie 				VARCHAR(100),
+				Nazwisko 			VARCHAR(100),
+				Kraj_zamieszkania 	VARCHAR(100),
+				Miasto 				VARCHAR(100),
+				Kod_pocztowy 		?,
+				Ulica 				VARCHAR(100)
+				Numer_domu 			INTEGER,
+				Data_urodzenia		DATE
+				PESEL 				VARCHAR(11),
+				Nr_telefonu			VARCHAR(20)
+)
+
+CREATE TABLE Przewodnicy(
+				Przewodnik_id 		SERIAL
+				Imie 				VARCHAR(100),
+				Nazwisko 			VARCHAR(100),
+				Adres_email 		VARCHAR(200),
+				Nr_telefonu 		VARCHAR(20)
+)
+
+CREATE TABLE Wycieczki(
+				Wycieczka_id 		SERIAL,
+				Liczba_uczestnikow 	INTEGER,
+				Data_rozpoczecia 	DATE,
+				Data_zakonczenia 	DATE,
+				Oferta_id 			INTEGER
+)
+
+CREATE TABLE Oferty(
+				Oferta_id 			SERIAL,
+				Miejsce_wyjazdu 	VARCHAR(100),
+				Limit_uczestnikow 	INTEGER,
+				Dlugosc_trwania 	INTEGER,
+				Cena_podstawowa 	DECIMAL(10,2),
+				Opis_oferty 		TEXT
+)
+
+CREATE TABLE Multimedia(
+				Multimedium_id 		SERIAL,
+				Zdjecie 			TEXT,
+				Oferta_id 			INTEGER
+)
+
+CREATE TABLE Tagi(
+				Tag_id 				SERIAL,
+				Nazwa_tagu 			VARCHAR(100),
+				Opis 				TEXT
+)
+
+CREATE TABLE Tagi_ofert(
+				Tag_id 				INTEGER,
+				Oferta_id 			INTEGER
+)
+
+CREATE TABLE Atrakcje(
+				Atrakcja_id 		SERIAL,
+				Nazwa_artakcji 		VARCHAR(250),
+				Czy_dla_dzieci 		BOOLEAN,
+				Opis_atrakcji 		TEXT
+)
+
+CREATE TABLE Atrakcje_w_ofercie(
+				Oferta_id 			INTEGER,
+				Atrakcja_id 		INTEGER
+)
+
+CREATE TABLE Przewodnictwa(
+				Przewodnik_id 		INTEGER,
+				Wycieczka_id 		INTEGER
+)
+
+CREATE TABLE Zamowienia(
+				Zamowienie_id 		SERIAL,
+				Klient_id 			INTEGER,
+				Wycieczka_id 		INTEGER,
+				Liczba_osob 		INTEGER,
+				Wartosc_zamowienia 	DECIMAL(10,2),
+				Klasa_oferty 		INTEGER,
+				Sposob_platnosci 	VARCHAR(100)
+)
+
+CREATE TABLE Klasy_ofert(
+				Klasa 				INTEGER,
+				Mnoznik 			DECIMAL(10,2),
+				Opis_slowny 		TEXT
+)
+
+CREATE TABLE Uczestnicy_w_zamowieniu(
+				Uczestnik_id 		INTEGER,
+				Zamowienie_id 		INTEGER
+)
