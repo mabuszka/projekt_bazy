d6769dff8bc64a6faf65970ad881a32ad9cc1909
mabuszka <magda@buszka.eu>
2021-01-14 12:21:18 +0100

    dodanie części więzów do uczestnicy
    dodanie więzów na wiek i kraj_zamieszkania

--- wiezy.sql
+++ wiezy.sql
@@ -1,2 +1,12 @@
 ALTER TABLE klasy_ofert ADD CHECK (mnoznik > 0);
 
+ALTER TABLE uczestnicy ADD CHECK (PESEL SIMILAR TO '[0-9]{11}');
+
+ALTER TABLE uczestnicy ADD CHECK (kraj_zamieszkania IN ('Polska', 'Francja', 'Niemcy' ));
+
+ALTER TABLE uczestnicy ADD CHECK (wiek >= 0 & wiek < 150);
+
+
+
+
+
