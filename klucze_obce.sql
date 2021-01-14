01f082fec7ee2546754ac825868c1aa1c1ca65de
mabuszka <magda@buszka.eu>
2021-01-14 11:59:41 +0100

    dodanie kluczy obcych w atrakcje_w_ofercie

--- klucze_obce.sql
+++ klucze_obce.sql
@@ -4,3 +4,15 @@ ALTER TABLE tagi_ofert ADD CONSTRAINT tagi_ofert_oferta FOREIGN KEY (oferta_id)
 ALTER TABLE tagi_ofert ADD CONSTRAINT tagi_ofert_tag FOREIGN KEY (tag_id)
 	REFERENCES tagi(tag_id) ON DELETE CASCADE ON UPDATE UPDATE;
 	
+ALTER TABLE przewodnictwa ADD CONSTRAINT przewodnictwa_przewodnik FOREIGN KEY (przewodnik_id)
+	REFERENCES przewodnicy(przewodnik_id) ON DELETE CASCADE ON UPDATE UPDATE;
+	
+ALTER TABLE przewodnictwa ADD CONSTRAINT przewodnictwa_wycieczka FOREIGN KEY (wycieczka_id)
+	REFERENCES wycieczki(wycieczka_id) ON DELETE CASCADE ON UPDATE UPDATE;
+	
+ALTER TABLE atrakcje_w_ofercie ADD CONSTRAINT atrakcje_w_ofercie_oferta FOREIGN KEY (oferta_id)
+	REFERENCES oferty(oferta_id) ON DELETE CASCADE ON UPDATE UPDATE;
+	
+ALTER TABLE atrakcje_w_ofercie ADD CONSTRAINT atrakcje_w_ofercie_atrakcja FOREIGN KEY (atrakcja_id)
+	REFERENCES atrakcje(atrakcja_id) ON DELETE CASCADE ON UPDATE UPDATE;
+	
