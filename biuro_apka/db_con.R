5c1847d6671b69a300a74dcd22bf551cd19b0870
zuzbrzo <zbrzoska22@gmail.com>
2021-01-29 19:35:59 +0100

    wspólna nazwa na bazę i użytkownika? :)

--- biuro_apka/db_con.R
+++ biuro_apka/db_con.R
@@ -4,3 +4,6 @@ library("RPostgres")
 #                  host = "localhost", port = 5432, 
 #                  user = "Magda", pass = "tajnehaslo")
 
+con <- dbConnect(RPostgres::Postgres(), dbname = "projekt_bazy_test",
+                 host = "localhost", port = 5432, 
+                 user = "studentka", pass = "tajnehaslo")
